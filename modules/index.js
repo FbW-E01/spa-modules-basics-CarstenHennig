3d61a4fd0f16323d702afca3afffaab73428a6d2
CarstenHennig <carsten.hennig@gmail.com>
2021-06-22 13:13:31 +0200

    Images and image folder missing

---
+++ modules/index.js
@@ -0,0 +1,8 @@
+import { hoverHeader as hover } from "./hover.js";
+hover.hoverHeader();
+
+import { scrollWindow as scroll } from "./scroll.js";
+scroll.scrollWindow();
+
+import { clickImages as click } from "./click.js";
+click.clickImages();
